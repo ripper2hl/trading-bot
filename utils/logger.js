c793045780967d061f50c91c2f1e93352a98a460
ripper2hl <israel-perales@outlook.com>
2026-08-16 14:42:58 -0600

    feat: add cyan color support to logger utility

--- utils/logger.js
+++ utils/logger.js
@@ -12,7 +12,8 @@ const colors = {
     green: '\x1b[32m%s\x1b[0m',
     red: '\x1b[31m%s\x1b[0m',
     gray: '\x1b[37m%s\x1b[0m',
-    yellow: '\x1b[33m%s\x1b[0m'
+    yellow: '\x1b[33m%s\x1b[0m',
+    cyan: '\x1b[36m%s\x1b[0m'
 }
 
 /**
